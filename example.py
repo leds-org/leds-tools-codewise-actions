e259c0da44ecd0cc4a76d1c789cffb62e71356ae
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2024-09-23 08:37:21 -0300

    testando

--- example.py
+++ example.py
@@ -5,7 +5,7 @@ def example_function():
         result.append(i * 2)  # Inefficient use of list append in a loop
     
     return result
-
+# colocando um comentário
 def unused_function():
     # Unused code example (code smell)
     return "This function is never called"
