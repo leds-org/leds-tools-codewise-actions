216beb6f8cd316624d3f85525a24d86f66ac190a
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2024-09-29 10:31:36 -0300

    criando um novo arquivo de teste

---
+++ example copy.py
@@ -0,0 +1,13 @@
+def example_function():
+    # A sample function with some performance issues and potential code smells.
+    result = []
+    for i in range(1000):
+        result.append(i * 2)  # Inefficient use of list append in a loop
+    
+    return result
+# colocando um comentário sei lã o que tem aqui 
+def unused_function():
+    # Unused code example (code smell)
+    return "This function is never called"
+## mais um comentário
+print(example_function())
